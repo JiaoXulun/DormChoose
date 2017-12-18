1c87b3cb65523bdbc8fa45831442276b2025866f
JiaoXulun <32259596+JiaoXulun@users.noreply.github.com>
2017-12-18 21:45:12 +0800

    0.0.2

--- app/src/main/java/cn/edu/pku/jiaoxulun/dormchoose/MainActivity.java
+++ app/src/main/java/cn/edu/pku/jiaoxulun/dormchoose/MainActivity.java
@@ -2,12 +2,18 @@ package cn.edu.pku.jiaoxulun.dormchoose;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public static String usr = null;
+    TextView txt_usr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txt_usr = (TextView)findViewById(R.id.usr_show);
+        txt_usr.setText(usr);
     }
 }
